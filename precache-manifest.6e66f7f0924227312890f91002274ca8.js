5dbd00b380764a0beb473dbf2052565e8fe68197
alfandy <brayandyi@gmail.com>
2019-10-18 23:57:30 +0800

    Updates

---
+++ precache-manifest.6e66f7f0924227312890f91002274ca8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c26b860acdf3b8438bcc1fb6b9accc",
+    "url": "/reactJs/index.html"
+  },
+  {
+    "revision": "850871ca261e447c8b1f",
+    "url": "/reactJs/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a15a6e412f0a967cef00",
+    "url": "/reactJs/static/js/2.e5442855.chunk.js"
+  },
+  {
+    "revision": "850871ca261e447c8b1f",
+    "url": "/reactJs/static/js/main.9bfffaa0.chunk.js"
+  },
+  {
+    "revision": "8a954d40064835f78285",
+    "url": "/reactJs/static/js/runtime-main.2bb8e8ee.js"
+  }
+]);
